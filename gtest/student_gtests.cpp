a75066d6cc43ed5940b887080c39231a4c14e038
Lucas Khun <lkhun@uci.edu>
2025-03-14 18:51:32 +0000

    Added test for print_word_ladder

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -2,7 +2,15 @@
 
 #include "dijkstras.h"
 #include "ladder.h"
+#include <sstream>
 
-TEST(Replace, Me) {
-  verify_word_ladder();
+TEST(TESTPrintLadder, test){
+  stringstream s;
+  vector<string> a {"a", "b", "c"};
+  std::streambuf *coutbuf = std::cout.rdbuf(s.rdbuf()); //save old buf
+
+  print_word_ladder(a);
+  EXPECT_TRUE(s.str() == "a→b→c→[END]\n");
+
+  std::cout.rdbuf(coutbuf);
 }
